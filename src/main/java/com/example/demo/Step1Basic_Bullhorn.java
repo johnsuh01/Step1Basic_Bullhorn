8acfe86f02630c27e447f41cce3a597002889fc7
johnsuh01 <johnsuh01@hotmail.com>
2019-12-13 14:56:03 -0500

    Step1Basic_Bullhorn

---
+++ src/main/java/com/example/demo/Step1Basic_Bullhorn.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step1Basic_Bullhorn {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Step1Basic_Bullhorn.class, args);
+    }
+
+}
